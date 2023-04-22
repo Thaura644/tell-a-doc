d9012d1b4d25a8ff071be23ee2a8151d920ae2dc
Benji Codes <89851307+Thaura644@users.noreply.github.com>
2023-04-22 18:49:51 +0300

    lets contribute

---
+++ client/src/components/Expert Care/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home(){
+
+    return (
+        <div>
+            <h1>Expert Care</h1>
+        </div>
+    )
+}
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
@@ -6,6 +7,23 @@ const HomePage = () => {
       <h1>Home Page</h1>
     </div>
   );
-};
 
+  
+
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Expert Care</Link></li>
+        <li><Link to="/for-individual">For Individual</Link></li>
+        <li><Link to="/for-organisation">For Organisation</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
+};
 export default HomePage;
